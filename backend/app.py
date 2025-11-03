8522b8ae8b6478f2efb41b77cf2ca0089d20f79b
Manalee Tamrakar <manalee0tamrakar@gmail.com>
2025-11-03 12:05:08 +0530

    Fix: Bind Flask app to 0.0.0 for Render

--- backend/app.py
+++ backend/app.py
@@ -108,5 +108,7 @@ def view_requests():
     })
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
